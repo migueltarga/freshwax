04e78586a0fc48af55544e1c0a4c9aab972b78ec
jasperboyd <jasper.boyd@ten24web.com>
2016-06-19 18:04:08 -0400

    setting up home page

--- laravel/resources/views/home/home.blade.php
+++ laravel/resources/views/home/home.blade.php
@@ -4,5 +4,10 @@
 	<header>
         <h1>Built For Your Music</h1>
     </header>
+    <p>
+        FRESHWAX will promote your music, tour, a new video, and even your label. <br />
+        FRESHWAX is built for musicians, by a musician. <br />
+        FRESHWAX is about independence. <br />
+    </p>
 </article>
 @stop
