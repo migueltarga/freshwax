aae6a264aae0dee96ecb8c166d94190d508aa533
jasperboyd <jasper.boyd@ten24web.com>
2016-06-29 18:07:51 -0400

    linking to artist detail in the listing

--- laravel/resources/views/artists/partials/show.blade.php
+++ laravel/resources/views/artists/partials/show.blade.php
@@ -1,25 +1,27 @@
+<a href="{{route('artists.show', $a->id)}}">
 <section class="four columns">
-				<h1>{{$a->name}}</h1>
-				<h2>{{$a->hometown}}</h2>
-				<p>{{$a->bio}}</p>
+                <h1>{{$a->name}}</h1>
+                <h2>{{$a->hometown}}</h2>
+                <p>{{$a->bio}}</p>
 
-				@foreach($a->photos as $p)
-					@if(!$p->banner && !$p->background)
-						<img src="{{$p->path}}" />
-					@endif
-				@endforeach
+                @foreach($a->photos as $p)
+                    @if(!$p->banner && !$p->background)
+                        <img src="{{$p->path}}" />
+                    @endif
+                @endforeach
 
-				@if($a->hasBanner())
-					<h3>Banner</h3>
-					<img src="{{$a->banner()->path}}">
-				@endif
+                @if($a->hasBanner())
+                    <h3>Banner</h3>
+                    <img src="{{$a->banner()->path}}">
+                @endif
 
-				@if($a->hasBackground())
-					<h3>Background</h3>
-					<img src="{{$a->background()->path}}">
-				@endif
+                @if($a->hasBackground())
+                    <h3>Background</h3>
+                    <img src="{{$a->background()->path}}">
+                @endif
 
-				@if(Auth::check() && isset($a->user) && Auth::user()->id == $a->user->id)
+                @if(Auth::check() && isset($a->user) && Auth::user()->id == $a->user->id)
                     @include('artists.partials.adminnav')
                 @endif
 </section>
+</a>
