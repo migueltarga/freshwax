bf1cfa66ee615168a5eea495ace35ff805d297e8
jasperboyd <jasper.boyd@ten24web.com>
2016-10-20 21:31:55 -0400

    home page markup

--- laravel/resources/views/home/home.blade.php
+++ laravel/resources/views/home/home.blade.php
@@ -6,10 +6,16 @@
             <h1>An Open Source Label</h1>
             <h2>Built For Your Music</h2>
         </header>
-        <p>
-            @include('layouts.partials.brand')  will promote your music, tour, a new video, and even your label. <br />
-            @include('layouts.partials.brand') is built for musicians, by a musician. <br />
-            @include('layouts.partials.brand') is about independence. <br />
+        <p class="l-3c">
+            <span class="c-1">
+                @include('layouts.partials.brand')  will promote your music, tour, a new video, and even your label. <br />
+            </span>
+            <span class="c-2">
+                @include('layouts.partials.brand') is built for musicians, by a musician. <br />
+            </span>
+            <span class="c-3">
+                @include('layouts.partials.brand') is about independence. <br />
+            </span>
         </p>
     </section>
     <section>
