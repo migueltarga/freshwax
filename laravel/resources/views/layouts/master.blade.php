d3f89a7a1827f37d0e7f5507e20f0f7f3cf73bcc
jasperboyd <jasperboyd@icloud.com>
2017-07-26 19:25:54 -0400

    adding bootstrap

--- laravel/resources/views/layouts/master.blade.php
+++ laravel/resources/views/layouts/master.blade.php
@@ -1,6 +1,8 @@
 <html>
 <head>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Vesper+Libre:700,900" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Yantramanav:100,400,900" rel="stylesheet">
 {!! HTML::style('styles/css/font-awesome.min.css') !!}
@@ -28,6 +30,8 @@
 
     @include('layouts.partials.footer')
 
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     {!! HTML::script('styles/js/jquery-2.1.3.min.js') !!}
     {!! HTML::script('styles/js/DateTimePicker.js') !!}
 
