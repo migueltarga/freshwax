3702a5e0a9a501011453dbe0ff46fd0dea809024
jasperboyd <jasperboyd@icloud.com>
2017-07-26 19:22:22 -0400

    spacing

--- laravel/resources/views/users/partials/form.blade.php
+++ laravel/resources/views/users/partials/form.blade.php
@@ -1,28 +1,28 @@
 @include('layouts.partials.form')
 @include('errors.partials.show')
 
-	<p>
-		{!!Form::label('name', 'Name:')!!}
-		{!!Form::text('name')!!}
-	</p>
+<p>
+    {!!Form::label('name', 'Name:')!!}
+    {!!Form::text('name')!!}
+</p>
 
-	<p>
-		{!!Form::label('email', 'Email:')!!}
-		{!!Form::text('email')!!}
-	</p>
-	<p>
-		{!!Form::label('password', 'Password:')!!}
-		{!!Form::password('password')!!}
-	</p>
+<p>
+    {!!Form::label('email', 'Email:')!!}
+    {!!Form::text('email')!!}
+</p>
+<p>
+    {!!Form::label('password', 'Password:')!!}
+    {!!Form::password('password')!!}
+</p>
 
-	<p>
-		{!!Form::label('password_confirmation', 'Confirm Password:')!!}
-		{!!Form::password('password_confirmation')!!}
-	</p>
+<p>
+    {!!Form::label('password_confirmation', 'Confirm Password:')!!}
+    {!!Form::password('password_confirmation')!!}
+</p>
 
-	@if(Auth::check() && Auth::user()->isadmin)
-		<p>
-			{!!Form::label('admin', 'Admin:')!!}
-			{!!Form::checkbox('admin')!!}
-		</p>
-	@endif
+@if(Auth::check() && Auth::user()->isadmin)
+    <p>
+        {!!Form::label('admin', 'Admin:')!!}
+        {!!Form::checkbox('admin')!!}
+    </p>
+@endif
