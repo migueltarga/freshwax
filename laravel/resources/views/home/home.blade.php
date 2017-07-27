6019a4414cf2ef7edc82a7a858422bb05750bbf6
jasperboyd <jasperboyd@icloud.com>
2017-07-26 21:19:38 -0400

    cleaning up home page a bit

--- laravel/resources/views/home/home.blade.php
+++ laravel/resources/views/home/home.blade.php
@@ -1,27 +1,35 @@
 @extends('layouts.master')
 @section('content')
-<article id="home">
-    <section>
-        <header>
+<article id="home" class="container-fluid">
+    <section class="row">
+        <header class="row jumbotron">
             <h1>An Open Source Label</h1>
             <h2>Built For Your Music</h2>
         </header>
-        <p class="l-3c">
-            <span class="c-1">
-                @include('layouts.partials.brand')  will promote your music, tour, a new video, and even your label. <br />
-            </span>
-            <span class="c-2">
-                @include('layouts.partials.brand') is built for musicians, by a musician. <br />
-            </span>
-            <span class="c-3">
-                @include('layouts.partials.brand') is about independence. <br />
-            </span>
-        </p>
+        <div class="row">
+            <div class="col-md-4">
+                <p>
+                    @include('layouts.partials.brand')  will promote your music, tour, a new video, and even your label. <br />
+                </p>
+            </div>
+            <div class="col-md-4">
+                <p>
+                    @include('layouts.partials.brand') is built for musicians, by a musician. <br />
+                </p>
+            </div>
+            <div class="col-md-4">
+                <p>
+                    @include('layouts.partials.brand') is about independence. <br />
+                </p>
+            </div>
+        </div>
     </section>
+    <!--
     <section>
         <header>
             <h1>What is @include('layouts.partials.brand')?</h1>
         </header>
     </section>
+    -->
 </article>
 @stop
