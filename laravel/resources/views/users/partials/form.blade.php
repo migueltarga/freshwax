affe078d782bd67195427d2ff795a0c9044a5fd1
jasperboyd <jasperboyd@icloud.com>
2017-07-26 21:33:18 -0400

    cleaning up the register view

--- laravel/resources/views/users/partials/form.blade.php
+++ laravel/resources/views/users/partials/form.blade.php
@@ -1,28 +1,29 @@
 @include('layouts.partials.form')
 @include('errors.partials.show')
 
-<p>
+<div class="form-group">
     {!!Form::label('name', 'Name:')!!}
-    {!!Form::text('name')!!}
-</p>
+    {!!Form::text('name','', array('class'=>'form-control'))!!}
+</div>
 
-<p>
+<div class="form-group">
     {!!Form::label('email', 'Email:')!!}
-    {!!Form::text('email')!!}
-</p>
-<p>
-    {!!Form::label('password', 'Password:')!!}
-    {!!Form::password('password')!!}
-</p>
+    {!!Form::text('email','', array('class'=>'form-control'))!!}
+</div>
 
-<p>
+<div class="form-group">
+    {!!Form::label('password', 'Password:')!!}
+    {!!Form::password('password', array('class'=>'form-control'))!!}
+</div>
+
+<div class="form-group">
     {!!Form::label('password_confirmation', 'Confirm Password:')!!}
-    {!!Form::password('password_confirmation')!!}
-</p>
+    {!!Form::password('password_confirmation', array('class'=>'form-control'))!!}
+</div>
 
 @if(Auth::check() && Auth::user()->isadmin)
-    <p>
+    <div class="form-group">
         {!!Form::label('admin', 'Admin:')!!}
         {!!Form::checkbox('admin')!!}
-    </p>
+    </div>
 @endif
