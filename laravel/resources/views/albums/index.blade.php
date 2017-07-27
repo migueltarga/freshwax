f5414be15b55c6f12b1d4cd3d7bae004acc74850
jasperboyd <jasperboyd@icloud.com>
2017-07-26 21:12:16 -0400

    cleaning up the albums page

--- laravel/resources/views/albums/index.blade.php
+++ laravel/resources/views/albums/index.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
-	<article class="twelve columns">
-		<header>
-			<h1>Discography</h1>
+	<article class="container-fluid">
+		<header class="jumbotron">
+			<h1>Albums</h1>
 		</header>
 		<?php $count = 0 ?>
 		@foreach($albums as $a)
 			@if($count % 3 == 0)
 				<div class="row">
 			@endif
-
-            @include('albums.partials.show')
-
+            <div class="col-md-4">
+                @include('albums.partials.show')
+            </div>
             @if(++$count % 3 == 0)
 				</div>
 			@endif
