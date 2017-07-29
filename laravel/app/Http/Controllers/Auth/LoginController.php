bb9faa01c95c0bbbe0b6195cbec5df541a2db520
jasperboyd <jasperboyd@icloud.com>
2017-07-29 17:26:08 -0400

    adding app namespace to new auth controllers

--- laravel/app/Http/Controllers/Auth/LoginController.php
+++ laravel/app/Http/Controllers/Auth/LoginController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Freshwax\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Freshwax\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
