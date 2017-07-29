e0d20251f29176406eab2a28188348f5bea9b897
jasperboyd <jasperboyd@icloud.com>
2017-07-29 17:11:47 -0400

    more fixes for the update to 5.4

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -4,15 +4,17 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 use Hash;
 
-class User extends Model implements AuthenticatableContract,
-                                    AuthorizableContract,
-                                    CanResetPasswordContract {
+class User extends Authenticatable implements AuthenticatableContract,
+                                                AuthorizableContract,
+                                                CanResetPasswordContract {
 
     use Authenticatable, Authorizable, CanResetPassword;
     /**
--- laravel/config/app.php
+++ laravel/config/app.php
@@ -83,7 +83,7 @@ return [
 
 	'key' => env('APP_KEY', 'SomeRandomString'),
 
-	'cipher' => MCRYPT_RIJNDAEL_128,
+    'cipher' => 'AES-128-CBC',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -202,7 +202,7 @@ return [
 		'View'      => 'Illuminate\Support\Facades\View',
 
         'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
 		'Image'     => 'Intervention\Image\Facades\Image'
 
 	],
