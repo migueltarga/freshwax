a63f82531f431716885a0c0478a34f1f13b78471
Jasper Boyd <jasper.boyd@ten24web.com>
2018-01-01 17:09:56 -0500

    fixing track delete

--- laravel/resources/views/tracks/partials/adminnav.blade.php
+++ laravel/resources/views/tracks/partials/adminnav.blade.php
@@ -6,7 +6,7 @@
 		<a href="{!! route('tracks.edit',  $track->id) !!}">
 			<i class="fa fa-pencil"></i>
 		</a>
-		<a href="{!! route('tracks.delete', 'Delete', $track->id) !!}">
+		<a href="{!! route('tracks.delete', $track->id) !!}">
 			<i class="fa fa-trash"></i>
 		</a>
 		<a href="{!! route('lyrics.create', $track->id) !!}">
