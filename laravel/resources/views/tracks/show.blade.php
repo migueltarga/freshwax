10507590d5c2ced0a582bd3969b2b17e489e7c14
Jasper Boyd <jasper.boyd@ten24web.com>
2018-01-02 20:42:38 -0500

    track page

--- laravel/resources/views/tracks/show.blade.php
+++ laravel/resources/views/tracks/show.blade.php
@@ -3,13 +3,26 @@
 @section('content')
     <article>
         <header class="row jumbotron">
-            <h1>{{$track->name}}</h1>
-            @if(isset($track->album))
-                <h2>{{$track->album->name}}</h2>
-                @endif
-                {{$track->path}}
-            {!!$track->soundcloud_embed!!}
-        </header>
-        @include('tracks.partials.adminnav')
+			<h1>{{$track->name}}</h1>
+
+			@if(isset($track->album))
+				<h2>{{$track->album->name}}</h2>
+			@endif
+		</header>
+
+		@include('tracks.partials.adminnav')
+
+		<section class="row">
+			@foreach($track->photos as $p)
+				@if(!$p->banner && !$p->background)
+					<img src="{{$p->path}}" class="img-responsive" />
+					@break
+				@endif
+			@endforeach
+			<audio controls>
+				<source src="{{ $track->path }}">
+			</audio>
+			{!! $track->soundcloud_embed !!}
+		</section>
 	</article>
 @stop
