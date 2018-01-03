61960aacc29edb8c15b53d8e37ce64fe1b35e75f
Jasper Boyd <jasper.boyd@ten24web.com>
2018-01-02 20:25:48 -0500

    track photo form

--- laravel/resources/views/photos/create/track.blade.php
+++ laravel/resources/views/photos/create/track.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.master')
 
 @section('content')
-	<article class="forms"> 
-		<header> 
-			<h1>Create Photo</h1>
-		</header> 
+	<article>
+		<header class="jumbotron">
+			<h1><i class="fa fa-plus"></i> Track Photo</h1>
+		</header>
 		{!!Form::open(['route'=>'photos.store', 'files' => 'true'])!!}
 			@include('photos.partials.form')
 			{!!Form::hidden('track', $id)!!}
-			{!!Form::submit('create')!!}
+			<button type="submit" class="btn btn-primary pull-right">
+				<i class="fa fa-plus"></i> Track Photo
+			</button>
 		{!!Form::close()!!}
 
-	</article> 
+	</article>
 @stop
