a5d8d262bd5ccafe88d1591184957cacb1ca83b7
Jasper Boyd <jasper.boyd@ten24web.com>
2018-02-10 14:47:17 -0500

    some cleanup on the tracks listing

--- laravel/resources/views/tracks/partials/show.blade.php
+++ laravel/resources/views/tracks/partials/show.blade.php
@@ -1,4 +1,4 @@
-<section class="four columns">
+<section>
 	<header>
 		<h1>{!!link_to_route('tracks.show', $track->name, $track->id)!!}</h1>
 
@@ -9,27 +9,26 @@
 		</h2>
 	</header>
 
-	<section class="row">
-		@foreach($track->photos as $p)
-			@if(!$p->banner && !$p->background)
-				<img src="{{$p->path}}" class="img-responsive" />
-				@break
-			@endif
-		@endforeach
-		<audio controls>
-			<source src="{{ $track->path }}">
-		</audio>
-		{!! $track->soundcloud_embed !!}
-	</section>
-
-	<h4>
-		@if($track->tags->count())
-			<ul>
-				@foreach($track->tags as $tag)
-					<li>{{$tag->tag}}</li>
-				@endforeach
-			</ul>
+	@foreach($track->photos as $p)
+		@if(!$p->banner && !$p->background)
+			<img src="{{$p->path}}" class="img-responsive" />
+			@break
 		@endif
-	</h4>
-    @include('tracks.partials.adminnav')
+	@endforeach
+
+	<audio controls>
+		<source src="{{ $track->path }}">
+	</audio>
+
+	{!! $track->soundcloud_embed !!}
+
+	@if($track->tags->count())
+		<ul>
+			@foreach($track->tags as $tag)
+				<li>{{$tag->tag}}</li>
+			@endforeach
+		</ul>
+	@endif
+
+	@include('tracks.partials.adminnav')
 </section>
