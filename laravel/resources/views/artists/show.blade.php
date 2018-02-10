640928681103549db5fc13d101a315e1ba1663f1
Jasper Boyd <jasper.boyd@ten24web.com>
2018-02-10 15:36:23 -0500

    working on artist detail page a bit

--- laravel/resources/views/artists/show.blade.php
+++ laravel/resources/views/artists/show.blade.php
@@ -4,19 +4,28 @@
 	<article class="row">
 
 		<header class="jumbotron col-md-12">
-			<h1>{{$a->name}}</h1>
-			<h2>{{$a->hometown}}</h2>
+			<div class="col-md-3">
+			@foreach($a->photos as $p)
+				<img src="{{$p->path}}" class="img-responsive img-circle" />
+				@break;
+			@endforeach
+			</div>
+
+			<div class="col-md-9">
+				<h1>{{$a->name}}</h1>
+				<h2>{{$a->hometown}}</h2>
+			</div>
 		</header>
 
 		@include('artists.partials.adminnav')
 
 		<div class="col-md-4">
-			<h3>Bio:</h3>
+			<h1>Bio:</h1>
 			<p>{{$a->bio}}</p>
 		</div>
 
 		<div class="col-md-4">
-			<h3>Albums:</h3>
+			<h1>Albums:</h1>
 			<ul>
 			@foreach($a->albums as $album)
 				<li>
@@ -29,7 +38,7 @@
 		</div>
 
 		<div class="col-md-4">
-			<h3>Tracks:</h3>
+			<h1>Tracks:</h1>
 			<ul>
 				@foreach($a->tracks as $t)
 					<li>
@@ -42,9 +51,7 @@
 		</div>
 
 		<div class="col-md-12">
-			@foreach($a->photos as $p)
-				<img src="{{$p->path}}" />
-			@endforeach
+
 		</div>
 	</article>
 @stop
