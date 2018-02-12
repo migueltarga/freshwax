11fcca3d709f285a85e5f5ad61e729bde234d6db
Jasper Boyd <jasper.boyd@ten24web.com>
2018-02-11 20:34:32 -0500

    some basic style updates

--- laravel/resources/views/layouts/partials/header.blade.php
+++ laravel/resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="row">
     <section class="col-md-12">
-        <h1>
+        <h1 class="site-header text-center">
             <a href="/">
                 @include('layouts.partials.brand')
             </a>
