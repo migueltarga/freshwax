fcae11bb39f468602181df4ec2417c2298d7e8f5
Jasper Boyd <jasper.boyd@ten24web.com>
2018-02-17 14:26:36 -0500

    adding a policy for tracks

---
+++ laravel/app/Policies/TrackPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace freshwax\Policies;
+
+use freshwax\Models\User;
+use freshwax\Track;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TrackPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the track.
+     *
+     * @param  \freshwax\Models\User  $user
+     * @param  \freshwax\Track  $track
+     * @return mixed
+     */
+    public function view(User $user, Track $track)
+    {
+		return true;
+	}
+
+    /**
+     * Determine whether the user can create tracks.
+     *
+     * @param  \freshwax\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->hasArtists();
+    }
+
+    /**
+     * Determine whether the user can update the track.
+     *
+     * @param  \freshwax\Models\User  $user
+     * @param  \freshwax\Track  $track
+     * @return mixed
+     */
+    public function update(User $user, Track $track)
+    {
+		return $track->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the track.
+     *
+     * @param  \freshwax\Models\User  $user
+     * @param  \freshwax\Track  $track
+     * @return mixed
+     */
+    public function delete(User $user, Track $track)
+    {
+		return $track->user_id === $user->id;
+   	}
+}
--- laravel/app/Providers/AuthServiceProvider.php
+++ laravel/app/Providers/AuthServiceProvider.php
@@ -2,26 +2,20 @@
 
 namespace Freshwax\Providers;
 
+use Freshwax\Track;
+use Freshwax\Policies\TrackPolicy;
+
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     *      * The policy mappings for the application.
-     *           *
-     *                * @var array
-     *                     */
+
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        Track::class => TrackPolicy::class,
     ];
 
-    /**
-     *      * Register any application authentication / authorization services.
-     *           *
-     *                * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
-     *                     * @return void
-     *                          */
+
     public function boot(GateContract $gate)
     {
         $this->registerPolicies($gate);
