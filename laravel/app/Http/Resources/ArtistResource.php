c97950faba2d0ce5e96d8f4c84491b3b1e89ec64
Jasper Boyd <jasper.boyd@ten24web.com>
2018-02-18 18:21:57 -0500

    adding an artist resource

---
+++ laravel/app/Http/Resources/ArtistResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace freshwax\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArtistResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+		return [
+			'id' => $this->id,
+			'name' => $this->name,
+			'hometown' => $this->hometown,
+			'bio' => $this->bio
+		];
+    }
+}
